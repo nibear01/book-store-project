12704cf7c32b27d860e620a39d281a953c98ca0b
bushra jahan <jbushra781@gmail.com>
2025-09-11 01:15:12 +0600

    added login page and updates app.jsx page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import Navbar from "./components/Navbar";
 import { Button } from "@/components/ui/button";
+import Login from "./pages/login";
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
         <Navbar />
+        <Login/>
+
+       
+       
+       
       </div>
     </Router>
   );
