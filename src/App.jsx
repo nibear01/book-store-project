ea25945a85c088127d85ddd05381f2818a6ce749
bushra jahan <jbushra781@gmail.com>
2025-09-11 03:06:24 +0600

    updating register form

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import Navbar from "./components/Navbar";
 import { Button } from "@/components/ui/button";
-import Login from "./pages/login";
+import Register from "./pages/register";
+import LoginPage from "./pages/LoginPage";
+import SignpPage from "./pages/SignupPage";
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
         <Navbar />
-        <Login/>
-
-       
-       
-       
+        <LoginPage/>
+        <SignpPage/>
       </div>
     </Router>
   );
---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,10 @@
+const ContactPage = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Contact Us</h1>
+      <p className="text-gray-600 mt-2">Get in touch with our team</p>
+    </div>
+  );
+};
+
+export default ContactPage;
