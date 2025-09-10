489da33fbb798f78d32f07cfd1718ca93ff7531b
Saifur Rahman <99525405+saif955@users.noreply.github.com>
2025-09-10 23:44:46 +0600

    Navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
-
+import Navbar from "./components/Navbar";
+import { Button } from "@/components/ui/button";
 function App() {
   return (
-    <>
-      <Homepage />
-    </>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+      </div>
+    </Router>
   );
 }
 
